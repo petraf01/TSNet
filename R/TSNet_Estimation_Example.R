dd45cd4cc41086c145691660ecc7670069c908c6
petraf01 <23476135+petraf01@users.noreply.github.com>
2018-03-22 17:07:42 +0000

    Update TSNet_Estimation_Example.R

--- R/TSNet_Estimation_Example.R
+++ R/TSNet_Estimation_Example.R
@@ -1,6 +1,6 @@
 ##### --- Code to generate synthetic data and implent TSNet for tumor-purity and co-expression networks estimation
 
-##### --------- Input Paramers for Synthetic Data Example
+##### --------- Input Paramers for Synthetic Data Generation 
  p=200;           # -- Number of genes (i.e., number of nodes in the network).
  rep=1;           # -- Number of independent data sets (i.e., replicates).
  n=100            # -- Number of observations for each replicate.
@@ -19,24 +19,25 @@
  meanP=NULL;    # -- Mean of the Beta distribution utilized to simulate tumor purity. When NULL, meanP=0.5.
  varP=NULL;     # -- Variance of the Beta distribution utilized to simulate tumor purity. When NULL, varP=0.04.
  
- # --- Load Function
+ # --- Load Functions used for Tumor purity and Network inference
  source(".../R/TSNet_function.R") 
 
- # --- Simulate Data
+ # --- Simulate Data using function Synthetic_data (check TSNet-manual for more information about this function)
  Data<-Synthetic_data(p=p,rep=rep,n=n,cluster=2,parameterseed=1,
                       dataseed=1,meanY=NULL, meanZ=NULL,sdY=NULL,sdZ=NULL, 
                       P.v=NULL,meanP=NULL,varP=NULL, sameModuleN=0,
                       betaModel=TRUE,topology=topology,betaSize=betaSize)
  
-# --- Estimate tumor purity
+# --- Estimate tumor purity using function deNet_purity (check TSNet-manual for more information about this function)
 
- purity<-deNet_purity(d$X[seq(1,n),],d$P[seq(1,n)])
+ purity<-deNet_purity(exprM=d$X[seq(1,n),],purity=d$P[seq(1,n)])
  print(paste("Correlation between true and estimated tumor purity = ",round(cor(purity$Epurity,d$realP),3)))
 
- # --- Estimate network
- rhoy=.25; rhoz=.25 # -- penalty parameters
+ # --- Estimate network using function deNet (check TSNet-manual for more information about this function)
+ rhoy=.25;  # -- penalty parameters for the tumor component 
+ rhoz=.25   # -- penalty parameters for the non-tumor component
 
  out<-deNet(t(d$X[seq(1,n),]),purity=purity$Epurity,paraM=purity$paraM,rhoy=rhoy,rhoz=rhoz)
 
- # ---- Evaluate network (TP and FP rate in estimating networks)
+ # ---- Evaluate network (TP and FP rate in estimating networks) (check TSNet-manual for more information about this function)
  Evaluate_network(out,d)
