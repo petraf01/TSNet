c74d14b1bb45dc02600ca45ec8351875bf7517ca
petraf01 <23476135+petraf01@users.noreply.github.com>
2018-03-22 17:04:06 +0000

    Update TSNet_Estimation_Example.R

--- R/TSNet_Estimation_Example.R
+++ R/TSNet_Estimation_Example.R
@@ -1,19 +1,24 @@
-##### --- R Code to implement TSNet on Synthetic Data Examples
+##### --- Code to generate synthetic data and implent TSNet for tumor-purity and co-expression networks estimation
 
 ##### --------- Input Paramers for Synthetic Data Example
- p=200;     # -- p: number of genes
- rep=1;     # -- rep: number of replicates
- n=100      # -- n: number of samples for each replicate
- cluster=4;  # -- cluster: number of clusters
- parameterseed=1
- dataseed=1
- meanY=NULL;meanZ=NULL;sdY=NULL;sdZ=NULL;
- P.v=NULL;meanP=NULL;varP=NULL; 
+ p=200;           # -- Number of genes (i.e., number of nodes in the network).
+ rep=1;           # -- Number of independent data sets (i.e., replicates).
+ n=100            # -- Number of observations for each replicate.
+ cluster=4;       # -- cluster: number of disjoint components in tumor and non-tumor networks
+ parameterseed=1  # -- Random seed for parameter sampling.
+ dataseed=1       # -- Random seed for observations sampling.
  sameModuleN=0         # -- sameModuleN: number of overlapping modules between tumor and normal components
  topology="Power";     # -- topology: which topology between "Star" and "Power" should be utilized to simulate tumor-specific and non-tumor specific networks 
  betaModel=TRUE;       # -- beta model: TRUE if prior purity is modeled as a beta distribution; FALSE if a logistic regression is utilized
  betaSize=1            # -- Variance parameter of the beta distribution
-
+ meanY=NULL;    # -- p dimensional vector containing the mean parameter of the Gaussian distribution of the gene expression in tumor component. When NULL, meanY=0
+ meanZ=NULL;    # -- p dimensional vector containing the mean parameter of the Gaussian distribution of the gene expression in non-tumor component. When NULL, meanZ=0
+ sdY=NULL;      # -- p dimensional vector containing the variance parameter of the Gaussian distribution of the gene expression in tumor component. When NULL, sdY=1
+ sdZ=NULL;      # -- p dimensional vector containing the variance parameter of the Gaussian distribution of the gene expression in non-tumor component. When NULL, sdZ=1      
+ P.v=NULL;      # -- True level of tumor-purity. When NULL, the value of tumor purity is simulated from a Beta distribution with mean meanP and variance varP.
+ meanP=NULL;    # -- Mean of the Beta distribution utilized to simulate tumor purity. When NULL, meanP=0.5.
+ varP=NULL;     # -- Variance of the Beta distribution utilized to simulate tumor purity. When NULL, varP=0.04.
+ 
  # --- Load Function
  source(".../R/TSNet_function.R") 
 
